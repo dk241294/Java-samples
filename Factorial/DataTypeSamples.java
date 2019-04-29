55e5201aa35ea19c5864387c20162a2519da323f
deepak kumar <deepak24121994@gmail.com>
2019-04-29 12:55:32 +0530

    java -samples

---
+++ Factorial/DataTypeSamples.java
@@ -0,0 +1,14 @@
+public class DataTypeSamples
+{ 
+public static void main(String[] args){
+int value=23;
+int myTotal=(value/2);
+System.out.println("my total"= + myTotal);
+byte myBytevalue=25;
+// byte myNewBytevalue=25/2;
+byte myNewBytevalue=(byte)25/2; // this is casting in java it by default change to int we have to specify datatype
+System.out.println("myNewBytevalue"= + myNewBytevalue);
+long myLongValue=52L;
+long myNewlongValue= long 52/6;
+System.out.println("myLongValue"= + myNewlongValue);
+}}
---
+++ OddOrEven/OddOrEven.java
@@ -0,0 +1,13 @@
+class OddOrEven
+{
+   public static void main(String args[])
+   {
+      int x;
+      for(x=1;x<20;x++){     
+      if (x % 2 == 0)
+         System.out.println("The number is even  "+x);
+      else
+         System.out.println("The number is odd  "+x);
+   }}
+}
+
